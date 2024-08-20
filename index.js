34bcd28456c78a54961be0c2c8aef6950b95e034
Beshoy <beshoy_mounir22@yahoo.com>
2024-08-20 20:18:56 +0300

    added Clock animation

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let i,
+    c = document.getElementById("outter");
+
 setInterval(() => {
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
@@ -17,6 +20,14 @@ setInterval(() => {
     if (seconds <= 9) {
         seconds = zero.concat(seconds);
     }
-
     time.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+    // Coloc counter
+
+    i = (seconds / 60) * 100;
+    let string = `conic-gradient(#ff9f0a ${i}%, black 0%)`;
+    c.style.backgroundImage = string;
+    if (i > 100) {
+        i = 0;
+    }
 }, 1000);
